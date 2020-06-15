a8facaa72816313f9613d7b7832dbd58dcc1a321
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-15 16:48:25 +0900

    Animating Views and Transitions

---
+++ SwiftUIExample/HikeView.swift
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2020, mycompany All rights reserved. 
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+
+    var body: some View {
+        VStack {
+            HStack {
+//                HikeGraph(data: hike, path: \.elevation)
+//                    .frame(width: 50, height: 30)
+
+                VStack(alignment: .leading) {
+                    Text(hike.name)
+                        .font(.headline)
+                    Text(hike.distanceText)
+                }
+
+                Spacer()
+
+                 Button(action: {
+                                   withAnimation {
+                                       self.showDetail.toggle()
+                                   }
+                               }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .padding()
+                        .animation(.easeInOut)
+                }
+            }
+
+            if showDetail {
+                HikeDetail(hike: hike)
+            }
+        }
+    }
+}
+
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+      HikeView(hike: hikeData[0])
+    }
+}
