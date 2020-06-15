e54ef748651c6890ff7c44866549dca3be2abd7f
H.K <16552133+bj1024@users.noreply.github.com>
2020-06-15 12:27:04 +0900

    Navigation and Data passing.

--- SwiftUIExample/LandmarkList.swift
+++ SwiftUIExample/LandmarkList.swift
@@ -5,16 +5,28 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
+  var body: some View {
+    NavigationView{
       List(landmarkData){ landmark in
-
-        LandmarkRow(landmark: landmark)
+        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+          LandmarkRow(landmark: landmark)
+        }
       }
+      .navigationBarTitle(
+        Text(/*@START_MENU_TOKEN@*/"Landmarks"/*@END_MENU_TOKEN@*/)
+      )
     }
+  }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
-    static var previews: some View {
-        LandmarkList()
+  static var previews: some View {
+    ForEach(["iPhone SE", "iPhone XS Max","iPad Pro (9.7-inch)"], id: \.self) { deviceName in
+      LandmarkList()
+        .previewDevice(PreviewDevice(rawValue: deviceName))
+
+        .previewDisplayName(deviceName)
     }
+
+  }
 }
--- SwiftUIExample/MapView.swift
+++ SwiftUIExample/MapView.swift
@@ -6,11 +6,13 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+  var coordinate: CLLocationCoordinate2D
+
+
   func makeUIView(context: Context) -> MKMapView {
     MKMapView(frame: .zero)
   }
   func updateUIView(_ uiView: MKMapView, context: Context) {
-    let coordinate = CLLocationCoordinate2D( latitude: 34.011286, longitude: -116.166868)
     let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
     let region = MKCoordinateRegion(center: coordinate, span: span)
     uiView.setRegion(region,animated:true)
@@ -19,6 +21,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
